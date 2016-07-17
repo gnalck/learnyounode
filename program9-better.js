762b9003510198cfe05c29cb1df2546da2a743ad
gnalck <gnalck@localhost.localdomain>
2016-07-17 16:01:04 -0700

    promises for 9

--- program9-better.js
+++ program9-better.js
@@ -1,31 +1,27 @@
+var Promise = require('es6-promise').Promise;
 var http = require('http');
 
-var res = {};
-var num = 3;
-var callback = function(i, data) {
-    res[i] = data;
-    if (--num == 0) {
-        for (var i = 0; i < 3; i++) {
-            console.log(res[i]);
-        }
-    }
-}
+var getresp = function(i) {
+    return new Promise(function(resolve, reject) {
+        http.get(process.argv[2+i], function(response) {
+            response.setEncoding('utf8');
 
-var getresp = function(callback, i) {
-    http.get(process.argv[2+i], function(response) {
-        response.setEncoding('utf8');
+            var acc = "";
+            response.on("data", function(data) {
+                acc += data;
+            });
 
-        var acc = "";
-        response.on("data", function(data) {
-            acc += data;
-        });
-
-        response.on("end", function() {
-            callback(i, acc);
+            response.on("end", function() {
+                resolve(acc);
+            });
         });
     });
-}
+};
 
+var res = [];
 for (var i = 0; i < 3; i++) {
-    getresp(callback, i);
+    res.push(getresp(i));
 }
+Promise.all(res).then(function(values) {
+    console.log(values.join("\n"));
+});
